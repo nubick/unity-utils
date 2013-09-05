50b72346434ee607f6f702a178e497ff5aae61de
Oleg Andrushko <nubick@gmail.com>
2013-09-05 11:49:49 +0800

    Added GameRandom class with methods Shuffle and ShuffleArray.

---
+++ sources/Assets/Scripts/Utils/GameRandom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts.Utils
+{
+	public static class GameRandom
+	{
+		public static void Shuffle<T>(this List<T> list)
+		{
+			T[] array = list.ToArray();
+			ShuffleArray(array);
+			list.Clear();
+			list.AddRange(array);
+		}
+
+		public static void ShuffleArray<T>(T[] array)
+		{
+			T[] source = new T[array.Length];
+			Array.Copy(array, source, array.Length);
+			for (int i = 1; i < array.Length; i++)
+			{
+				int indRnd = Random.Range(0, i + 1);
+				array[i] = array[indRnd];
+				array[indRnd] = source[i];
+			}
+		}
+	}
+}
