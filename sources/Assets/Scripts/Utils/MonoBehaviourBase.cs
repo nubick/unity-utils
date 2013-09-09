da71be4c76f5cd0def5901bf8ca1835ac62a9085
Oleg Andrushko <nubick@gmail.com>
2013-09-09 11:26:26 +0800

    Instantiate method parameters.

--- sources/Assets/Scripts/Utils/MonoBehaviourBase.cs
+++ sources/Assets/Scripts/Utils/MonoBehaviourBase.cs
@@ -5,6 +5,7 @@ namespace Assets.Scripts.Utils
 	public abstract class MonoBehaviourBase : MonoBehaviour
 	{
 		private Transform _transform;
+
 		public Transform Transform
 		{
 			get
@@ -17,7 +18,17 @@ namespace Assets.Scripts.Utils
 
 		protected T Instantiate<T>(Object obj) where T : Object
 		{
-			return (T)Instantiate(obj);
+			return (T) Instantiate(obj);
+		}
+
+		protected T Instantiate<T>(Object obj, Vector3 position) where T : Object
+		{
+			return (T) Instantiate(obj, position, new Quaternion());
+		}
+
+		protected T Instantiate<T>(Object obj, Vector3 position, Quaternion rotation) where T : Object
+		{
+			return (T) Instantiate(obj, position, rotation);
 		}
 
 	}
