83eecaf811dd69fd6ba2968a730e31df4aa76b49
Oleg Andrushko <nubick@gmail.com>
2014-02-22 19:39:59 +0800

    Sorting layer settings component.

---
+++ sources/Assets/Scripts/Editor/SortingLayerSettingsEditor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using Assets.Scripts.Utils;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Assets.Scripts.Editor
+{
+    [CustomEditor(typeof(SortingLayerSettings))]
+    public class SortingLayerSettingsEditor : UnityEditor.Editor
+    {
+        private Renderer _renderer;
+        private string[] _sortingLayerNames;
+
+        public void OnEnable()
+        {
+            _renderer = (target as SortingLayerSettings).renderer;
+            _sortingLayerNames = GetSortingLayerNames();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (_renderer == null)
+            {
+                EditorGUILayout.LabelField("Game Object doesn't have renderer.");
+            }
+            else
+            {
+                int ind = Mathf.Max(0, Array.IndexOf(_sortingLayerNames, _renderer.sortingLayerName));
+                int changedInd = EditorGUILayout.Popup("Sorting Layer", ind, _sortingLayerNames);
+                if (changedInd != ind)
+                {
+                    Undo.RecordObject(_renderer, "Edit Sorting Layer Name");
+                    _renderer.sortingLayerName = _sortingLayerNames[changedInd];
+                    EditorUtility.SetDirty(_renderer);
+                }
+
+                int sortingOrder = _renderer.sortingOrder;
+                int changedSortingOrder = EditorGUILayout.IntField("Order in Layer", sortingOrder);
+                if (changedSortingOrder != sortingOrder)
+                {
+                    Undo.RecordObject(_renderer, "Edit Sorting Order");
+                    _renderer.renderer.sortingOrder = changedSortingOrder;
+                    EditorUtility.SetDirty(_renderer);
+                }
+            }
+        }
+
+        public string[] GetSortingLayerNames()
+        {
+            Type internalEditorUtilityType = typeof(InternalEditorUtility);
+            PropertyInfo sortingLayersProperty = internalEditorUtilityType.GetProperty(
+                "sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
+            return (string[])sortingLayersProperty.GetValue(null, new object[0]);
+        }
+    }
+}
---
+++ sources/Assets/Scripts/Utils/SortingLayerSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Scripts.Utils
+{
+    public class SortingLayerSettings : MonoBehaviourBase
+    {
+        //This is wrapper for creating SortingLayerSettingsEditor where we can change setting in rendrer
+    }
+}
