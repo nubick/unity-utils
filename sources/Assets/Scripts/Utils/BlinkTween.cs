b2a5af31ac67880149e45fa8a7c945347735d9b5
Oleg Andrushko <nubick@gmail.com>
2014-03-13 17:38:25 +0800

    Added new script: BlinkTween

---
+++ sources/Assets/Scripts/Utils/BlinkTween.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Utils
+{
+    public class BlinkTween : MonoBehaviour
+    {
+        public float BlinkDelay;
+
+        public void Start()
+        {
+            StartCoroutine(BlinkCoroutine());
+        }
+
+        private IEnumerator BlinkCoroutine()
+        {
+            for (;;)
+            {
+                yield return new WaitForSeconds(BlinkDelay);
+                renderer.enabled = false;
+                yield return new WaitForSeconds(BlinkDelay);
+                renderer.enabled = true;
+            }
+        }
+
+    }
+}
