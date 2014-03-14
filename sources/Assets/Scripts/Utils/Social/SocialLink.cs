e9eb745ea5dfcf5b379b10fa6eb6530408d6cec6
Oleg Andrushko <nubick@gmail.com>
2014-03-14 17:21:49 +0800

    SocialLink script
    Open twitter of facebook application by url on iOS devices.

---
+++ sources/Assets/Scripts/Utils/Social/SocialLink.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Utils.Social
+{
+    /// <summary>
+    /// Try to open facebook or twitter application on device. If such application is not installed try to open
+    /// default browser.
+    ///
+    /// Facebook: 
+    /// URL: https://www.facebook.com/pages/name/id
+    /// AppURL: fb://profile/id
+    ///
+    /// Twitter:
+    /// URL: https://www.twitter.com/name
+    /// AppURL: twitter://user?screen_name=name
+    /// </summary>
+    public class SocialLink : MonoBehaviourBase
+    {
+        private bool _isLeftApp;
+
+        public string URL;
+        public string AppURL;
+        public float WaitingHackDelay;
+
+        public void OpenUrl()
+        {
+            StartCoroutine(OpenUrlCoroutine());
+        }
+
+        private IEnumerator OpenUrlCoroutine()
+        {
+            _isLeftApp = false;
+            Application.OpenURL(AppURL);
+            yield return new WaitForSeconds(WaitingHackDelay);
+            if (!_isLeftApp)
+                Application.OpenURL(URL);
+        }
+
+        public void OnApplicationPause()
+        {
+            _isLeftApp = true;
+        }
+
+    }
+}
