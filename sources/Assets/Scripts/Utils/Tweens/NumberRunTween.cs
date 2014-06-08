6300299e70810259bcfaec71ddb7bb7714089f3f
Oleg Andrushko <nubick@gmail.com>
2014-06-08 21:37:53 +0800

    NumberRunTween

---
+++ sources/Assets/Scripts/Utils/Tweens/NumberRunTween.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Utils.Tweens
+{
+    public class NumberRunTween : MonoBehaviourBase
+    {
+        private int _from;
+        private int _to;
+        private float _duration;
+        private Action<int> _updateDelegate;
+        private float _startTime;
+
+        public static void Run(GameObject item, int from, int to, float duration, Action<int> updateDelegate)
+        {
+            NumberRunTween tween = item.GetComponent<NumberRunTween>();
+            if (tween == null)
+                tween = item.AddComponent<NumberRunTween>();
+            else
+                tween.StopAllCoroutines();
+            tween.Run(from, to, duration, updateDelegate);
+        }
+
+        private void Run(int from, int to, float duration, Action<int> updateDelegate)
+        {
+            _startTime = Time.time;
+            _from = from;
+            _to = to;
+            _duration = duration;
+            _updateDelegate = updateDelegate;
+        }
+
+        public void Update()
+        {
+            float t = (Time.time - _startTime)/_duration;
+            if (t > 1.0f)
+            {
+                _updateDelegate(_to);
+                Destroy(this);
+            }
+            else
+            {
+                _updateDelegate((int)Mathf.Lerp(_from, _to, t));
+            }
+        }
+    }
+}
