3978854c9b648ed9a0ecaf70e18bc01aa9e00497
Oleg Andrushko <nubick@gmail.com>
2015-03-26 15:45:26 +0700

    NextItem and Take methods added to GameRandom.

--- sources/Assets/Scripts/Utils/GameRandom.cs
+++ sources/Assets/Scripts/Utils/GameRandom.cs
@@ -39,18 +39,56 @@ namespace Assets.Scripts.Utils
 	        return min + Random.Range(0, count)*(max - min)/(count - 1);
 	    }
 
-        public static int NextWeightedInd(int[] weights)
-        {
-            int randomInt = Random.Range(1, weights.Sum() + 1);
-            int sum = 0;
-            for (int i = 0; i < weights.Length; i++)
-            {
-                sum += weights[i];
-                if (randomInt <= sum)
-                    return i;
-            }
-            throw new Exception("Logic error!");
-        }
+		public static int NextWeightedInd(int[] weights)
+		{
+			return NextWeightedInd(weights.Select(i => (float)i).ToArray());
+		}
+
+		public static int NextWeightedInd(float[] weights)
+		{
+			float random = Random.Range(0f, weights.Sum());
+			float sum = 0f;
+			for (int i = 0; i < weights.Length; i++)
+			{
+				sum += weights[i];
+				if (random <= sum)
+					return i;
+			}
+			throw new Exception("Logic error!");
+		}
+
+		/// <summary>
+		/// Return random item from array.
+		/// </summary>
+		public static T NextItem<T>(T[] array)
+		{
+			return array[Random.Range(0, array.Length)];
+		}
+
+		/// <summary>
+		/// Return random item from list.
+		/// </summary>
+		public static T NextItem<T>(List<T> list)
+		{
+			return list[Random.Range(0, list.Count)];
+		}
+
+		/// <summary>
+		/// Return list of random items from list.
+		/// </summary>
+		public static List<T> Take<T>(List<T> list, int count)
+		{
+			List<T> items = new List<T>();
+			List<int> remainedIndexes = Enumerable.Range(0, list.Count).ToList();
+			for (int i = 0; i < count; i++)
+			{
+				int selectedIndex = NextItem(remainedIndexes);
+				remainedIndexes.Remove(selectedIndex);
+				items.Add(list[selectedIndex]);
+			}
+			return items;
+		}
+
 
 	}
 }
