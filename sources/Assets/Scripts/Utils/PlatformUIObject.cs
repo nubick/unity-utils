5d982e41d2ce403203331e73a5cb5b68c408ba74
Oleg Andrushko <nubick@gmail.com>
2015-04-22 19:37:21 +0700

    PlatformUIObject bug-fix for iOS.

--- sources/Assets/Scripts/Utils/PlatformUIObject.cs
+++ sources/Assets/Scripts/Utils/PlatformUIObject.cs
@@ -1,22 +1,23 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts.Utils
 {
     public class PlatformUIObject : MonoBehaviour
     {
-	    public RuntimePlatform[] Platforms;
-        public bool IsForceVisible;
+		public List<RuntimePlatform> Platforms;
+		public bool IsForceVisible;
 
-        public void OnEnable()
-        {
-            UpdateVisibility();
-        }
+		public void OnEnable()
+		{
+			UpdateVisibility();
+		}
 
-        private void UpdateVisibility()
-        {
-	        gameObject.SetActive(IsForceVisible || Platforms.Contains(Application.platform));
-        }
+		private void UpdateVisibility()
+		{
+			gameObject.SetActive(IsForceVisible || Platforms.Contains(Application.platform));
+		}
     }
 
 }
