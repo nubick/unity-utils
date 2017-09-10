99f664508ee5c12818d11940592b5dceec4f4d57
Oleg Andrushko <nubick@gmail.com>
2017-09-10 17:46:00 +0800

    RandomUtil refactoring and improvements.

--- sources/Assets/Scripts/Utils/RandomUtil.cs
+++ sources/Assets/Scripts/Utils/RandomUtil.cs
@@ -16,6 +16,23 @@ namespace Assets.Scripts.Utils
 		}
 
 		/// <summary>
+		/// Return random item from item1 and item2 set.
+		/// </summary>
+		public static T Next<T>(T item1, T item2)
+		{
+			return NextBool() ? item1 : item2;
+		}
+
+		/// <summary>
+		/// Return random item from item1, item2 and item3 set.
+		/// </summary>
+		public static T Next<T>(T item1, T item2, T item3)
+		{
+			int n = Random.Range(0, 3);
+			return n == 0 ? item1 : (n == 1 ? item2 : item3);
+		}
+
+		/// <summary>
 		/// Return random item from array.
 		/// </summary>
 		public static T NextItem<T>(T[] array)
@@ -41,11 +58,19 @@ namespace Assets.Scripts.Utils
 		}
 
         /// <summary>
-        /// Return random index of passed array. Index random selection is based on array weights;
+        /// Return random index of passed array. Index random selection is based on array weights.
         /// </summary>
 		public static int NextWeightedInd(int[] weights)
 		{
-			return NextWeightedInd(weights.Select(i => (float)i).ToArray());
+            int randomPoint = Random.Range(0, weights.Sum());
+			int sum = 0;
+			for (int i = 0; i < weights.Length; i++)
+			{
+				sum += weights[i];
+				if (randomPoint <= sum)
+					return i;
+			}
+			throw new Exception("Logic error!");
 		}
 
         /// <summary>
@@ -53,50 +78,19 @@ namespace Assets.Scripts.Utils
         /// </summary>
 		public static int NextWeightedInd(float[] weights)
 		{
-			float random = Random.Range(0f, weights.Sum());
+			float randomPoint = Random.Range(0f, weights.Sum());
 			float sum = 0f;
 			for (int i = 0; i < weights.Length; i++)
 			{
 				sum += weights[i];
-				if (random <= sum)
+				if (randomPoint <= sum)
 					return i;
 			}
 			throw new Exception("Logic error!");
 		}
 
-		public static float NextDiscrete(float min, float max, int count)
-		{
-			if (count < 2) return min;
-			return min + Random.Range(0, count) * (max - min) / (count - 1);
-		}
-
-        /// <summary>
-        /// Shuffly list of items.
-        /// </summary>
-		public static void Shuffle<T>(this List<T> list)
-		{
-			T[] array = list.ToArray();
-			ShuffleArray(array);
-			list.Clear();
-			list.AddRange(array);
-		}
-
-        /// <summary>
-        /// Shuffle array.
-        /// </summary>
-		public static void ShuffleArray<T>(T[] array)
-		{
-			for (int i = 1; i < array.Length; i++)
-			{
-				int indRnd = Random.Range(0, i + 1);
-                T temp = array[i];
-				array[i] = array[indRnd];
-				array[indRnd] = temp;
-			}
-		}
-
 		/// <summary>
-		/// Return list of random items from list.
+		/// Return sub-list of random items from origin list without repeating.
 		/// </summary>
 		public static List<T> Take<T>(List<T> list, int count)
 		{
@@ -111,22 +105,32 @@ namespace Assets.Scripts.Utils
 			return items;
 		}
 
-        /// <summary>
-        /// Return random item from item1 and item2 set.
-        /// </summary>
-        public static T Next<T>(T item1, T item2)
-        {
-            return NextBool() ? item1 : item2;
-        }
+		/// <summary>
+		/// Shuffle list of items.
+		/// </summary>
+		public static void Shuffle<T>(this List<T> list)
+		{
+            for (int i = 1; i < list.Count; i++)
+			{
+				int indRnd = Random.Range(0, i + 1);
+				T temp = list[i];
+				list[i] = list[indRnd];
+				list[indRnd] = temp;
+			}
+		}
 
         /// <summary>
-        /// Return random item from item1, item2 and item3 set.
+        /// Shuffle array of items.
         /// </summary>
-        public static T Next<T>(T item1, T item2, T item3)
-        {
-            int n = Random.Range(0, 3);
-            return n == 0 ? item1 : (n == 1 ? item2 : item3);
-        }
-
+		public static void Shuffle<T>(T[] array)
+		{
+			for (int i = 1; i < array.Length; i++)
+			{
+				int indRnd = Random.Range(0, i + 1);
+                T temp = array[i];
+				array[i] = array[indRnd];
+				array[indRnd] = temp;
+			}
+		}
 	}
 }
