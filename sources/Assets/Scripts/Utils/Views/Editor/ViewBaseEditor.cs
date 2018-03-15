6cad8511fbbf89fc4b816d9b5618e02591a80b6a
Oleg Andrushko <nubick@gmail.com>
2018-03-15 21:15:07 +0700

    Views: Show/Hide buttons for Views in Inspector for debug purposes in Playing mode.

---
+++ sources/Assets/Scripts/Utils/Views/Editor/ViewBaseEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Utils.Views.Editor
+{
+    [CustomEditor(typeof(ViewBase), true)]
+    public class ViewBaseEditor : UnityEditor.Editor
+    {
+        private ViewBase View { get { return target as ViewBase; } }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (EditorApplication.isPlaying)
+                DrawRuntimeEditorActions();
+        }
+
+        private void DrawRuntimeEditorActions()
+        {
+            GUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Show"))
+                View.Show();
+
+            if (GUILayout.Button("Hide"))
+                View.Hide();
+
+            GUILayout.EndHorizontal();
+        }
+    }
+}
