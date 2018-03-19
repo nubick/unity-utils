9f2d9a180a1abc486448c68394b842f4f7d833b7
Oleg Andrushko <nubick@gmail.com>
2018-03-19 14:32:36 +0700

    CallMethodScript: parameters value input possibility (int, string, float, unity Object).

--- sources/Assets/Scripts/Utils/Editor/CallMethodScriptEditor.cs
+++ sources/Assets/Scripts/Utils/Editor/CallMethodScriptEditor.cs
@@ -18,19 +18,22 @@ namespace Assets.Scripts.Editor
 
             Component component = DrawSelectComponent();
 
-            bool isShowPrivateNew = EditorGUILayout.Toggle("Private Methods", Target.IsShowPrivate);
+            bool isShowPrivateNew = EditorGUILayout.Toggle("Include Private Methods", Target.IsShowPrivate);
             if (isShowPrivateNew != Target.IsShowPrivate)
             {
                 Target.IsShowPrivate = isShowPrivateNew;
                 EditorUtility.SetDirty(Target);
             }
 
-            DrawSelectMethod(component);
+            MethodInfo methodInfo = DrawSelectMethod(component);
+            if (methodInfo == null)
+                return;
+
+            DrawInputParameter(methodInfo);
 
             if (GUILayout.Button("Execute"))
             {
-                MethodInfo methodInfo = component.GetType().GetMethods(GetFilter())[Target.SelectedMethodIndex];
-                methodInfo.Invoke(component, null);
+                methodInfo.Invoke(component, Target.ParamterValues);
             }
 
             GUILayout.EndVertical();
@@ -49,23 +52,115 @@ namespace Assets.Scripts.Editor
             return components[Target.SelectedComponentIndex];
         }
 
-        private void DrawSelectMethod(Component component)
+        private MethodInfo DrawSelectMethod(Component component)
         {
+            MethodInfo selectedMethodInfo = null;
             MethodInfo[] methodInfos = component.GetType().GetMethods(GetFilter());
-            string[] methodNames = methodInfos.Select(_ => _.Name).ToArray();
-            int newMethodIndex = EditorGUILayout.Popup("Method", Target.SelectedMethodIndex, methodNames);
-            if (newMethodIndex != Target.SelectedMethodIndex)
+            if (methodInfos.Any())
             {
-                Target.SelectedMethodIndex = newMethodIndex;
-                EditorUtility.SetDirty(Target);
+                string[] methodNames = BuildMethodNames(methodInfos);
+                int oldMethodIndex = Target.SelectedMethodIndex > methodInfos.Length - 1 ? 0 : Target.SelectedMethodIndex;
+                int newMethodIndex = EditorGUILayout.Popup("Method", oldMethodIndex, methodNames);
+                if (newMethodIndex != Target.SelectedMethodIndex)
+                {
+                    Target.SelectedMethodIndex = newMethodIndex;
+                    EditorUtility.SetDirty(Target);
+                }
+                selectedMethodInfo = methodInfos[Target.SelectedMethodIndex];
             }
+            return selectedMethodInfo;
+        }
+
+        private string[] BuildMethodNames(MethodInfo[] methodInfos)
+        {
+            string[] methodNames = new string[methodInfos.Length];
+            for (int i = 0; i < methodInfos.Length; i++)
+            {
+                MethodInfo methodInfo = methodInfos[i];
+                ParameterInfo[] parameterInfos = methodInfo.GetParameters();
+                List<string> typeStrings = new List<string>();
+                foreach(ParameterInfo parameterInfo in parameterInfos)
+                {
+                    if (parameterInfo.ParameterType == typeof(string))
+                        typeStrings.Add("str");
+                    else if (parameterInfo.ParameterType == typeof(int))
+                        typeStrings.Add("int");
+                    else if (parameterInfo.ParameterType == typeof(float))
+                        typeStrings.Add("float");
+                    else if (parameterInfo.ParameterType.IsSubclassOf(typeof(Object)))
+                        typeStrings.Add("uobj");
+                    else
+                        typeStrings.Add("not-supported");                    
+                }
+                string aggregatedTypes = typeStrings.Any() ? typeStrings.Aggregate((s1, s2) => s1 + "," + s2) : string.Empty;
+                methodNames[i] = string.Format("{0}({1})", methodInfos[i].Name, aggregatedTypes);
+            }
+            return methodNames;
         }
 
         private BindingFlags GetFilter()
         {
-            BindingFlags filter = BindingFlags.Instance | BindingFlags.DeclaredOnly;
-            filter |= Target.IsShowPrivate ? BindingFlags.NonPublic : BindingFlags.Public;
+            BindingFlags filter = BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public;
+            if (Target.IsShowPrivate)
+                filter |= BindingFlags.NonPublic;
             return filter;
+        }
+
+        private void DrawInputParameter(MethodInfo methodInfo)
+        {
+            ParameterInfo[] parameterInfos = methodInfo.GetParameters();
+
+            if (Target.ParamterValues == null || Target.ParamterValues.Length != parameterInfos.Length)
+                Target.ParamterValues = new object[parameterInfos.Length];
+
+            for (int i = 0; i < parameterInfos.Length; i++)
+            {
+                if (parameterInfos[i].ParameterType == typeof(string))
+                {
+                    string oldValue = Target.ParamterValues[i] is string ? (string)Target.ParamterValues[i] : string.Empty;
+                    string newValue = EditorGUILayout.TextField("String parameter", oldValue);
+                    if (newValue != oldValue)
+                    {
+                        Target.ParamterValues[i] = newValue;
+                        EditorUtility.SetDirty(Target);
+                    }
+                }
+                else if (parameterInfos[i].ParameterType == typeof(int))
+                {
+                    int oldValue = Target.ParamterValues[i] is int ? (int)Target.ParamterValues[i] : 0;
+                    int newValue = EditorGUILayout.IntField("Integer parameter", oldValue);
+                    if (newValue != oldValue)
+                    {
+                        Target.ParamterValues[i] = newValue;
+                        EditorUtility.SetDirty(Target);
+                    }
+                }
+                else if (parameterInfos[i].ParameterType == typeof(float))
+                {
+                    float oldValue = Target.ParamterValues[i] is float ? (float)Target.ParamterValues[i] : 0f;
+                    float newValue = EditorGUILayout.FloatField("Float parameter", oldValue);
+                    if (!Mathf.Approximately(oldValue, newValue))
+                    {
+                        Target.ParamterValues[i] = newValue;
+                        EditorUtility.SetDirty(Target);
+                    }
+                }
+                else if (parameterInfos[i].ParameterType.IsSubclassOf(typeof(Object)))
+                {
+                    System.Type parameterType = parameterInfos[i].ParameterType;
+
+                    Object oldValue = null;
+                    if (Target.ParamterValues[i] != null && Target.ParamterValues[i].GetType() == parameterType)
+                        oldValue = Target.ParamterValues[i] as Object;
+
+                    Object newValue = EditorGUILayout.ObjectField("Object parameter", oldValue, parameterType, true);
+                    if (newValue != oldValue)
+                    {
+                        Target.ParamterValues[i] = newValue;
+                        EditorUtility.SetDirty(Target);
+                    }
+                }
+            }
         }
     }
 }
