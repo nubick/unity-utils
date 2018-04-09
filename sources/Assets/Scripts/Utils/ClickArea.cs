56d833a510cf092c68e58285d05427c6b501e216
Oleg Andrushko <nubick@gmail.com>
2018-04-09 19:27:55 +0700

    ClickArea for Unity UI.

---
+++ sources/Assets/Scripts/Utils/ClickArea.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.UI;
+
+namespace Assets.Scripts.Utils
+{
+    public class ClickArea : Graphic
+    {
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+        }
+    }
+}
---
+++ sources/Assets/Scripts/Utils/Editor/ClickAreaEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Assets.Scripts.Utils.Editor
+{
+    [CustomEditor(typeof(ClickArea))]
+    public class ClickAreaEditor : UnityEditor.Editor
+    {
+        //Hide any additional settings in ClickArea component. I don't need color or raycast target. They don't have sense here.
+        public override void OnInspectorGUI()
+        {
+        }
+    }
+}
