9eb0c6ea5c808872ae1fd89347825f8a689b0023
Oleg Andrushko <nubick@gmail.com>
2018-04-17 12:55:57 +0700

    SoundEffect as ScriptableObject was introduced.

---
+++ sources/Assets/Scripts/Utils/Editor/SoundEffectEditor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Assets.Scripts.Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Editor
+{
+    [CustomEditor(typeof(SoundEffect))]    
+    public class SoundEffectEditor : UnityEditor.Editor
+    {
+        private SoundEffect SoundEffect { get { return target as SoundEffect; } }
+
+        public void OnEnable()
+        {
+            Play();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            GUILayout.BeginVertical();
+
+            SoundEffect.Type = (SoundEffectType)EditorGUILayout.EnumPopup("Type", SoundEffect.Type);
+
+            switch(SoundEffect.Type)
+            {
+                case SoundEffectType.Single:
+                    SoundEffect.AudioClip = EditorGUILayout.ObjectField("Audio Clip", SoundEffect.AudioClip, typeof(AudioClip), false) as AudioClip;
+                    break;
+                case SoundEffectType.Multiple:
+                    SoundEffect.SelectionType = (SoundEffectSelectionType)EditorGUILayout.EnumPopup("Selection", SoundEffect.SelectionType);
+                    SerializedProperty serializedProperty = serializedObject.FindProperty("AudioClips");
+                    EditorGUILayout.PropertyField(serializedProperty, true);
+                    serializedObject.ApplyModifiedProperties();
+                    break;
+                default:
+                    throw new Exception("Not supported SoundEffectType: " + SoundEffect.Type);
+            }
+
+            SoundEffect.Volume = EditorGUILayout.Slider("Volume", SoundEffect.Volume, 0f, 1f);
+
+            if (GUILayout.Button("Play"))
+                Play();
+
+            GUILayout.EndVertical();
+
+            EditorUtility.SetDirty(SoundEffect);
+        }
+
+        private void Play()
+        {
+            AudioSource audioSource = FindObjectOfType<AudioSource>();
+            if (audioSource == null)
+                Debug.Log("Can't find AudioSource in the opened scene to play Sound Effect.");
+            else
+                SoundEffect.Play(audioSource);            
+        }
+    }
+}
---
+++ sources/Assets/Scripts/Utils/SoundEffect.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Assets.Scripts.Utils;
+using UnityEngine;
+
+namespace Assets.Scripts.Data
+{
+    [CreateAssetMenu]
+    public class SoundEffect : ScriptableObject
+    {
+        private int _nextAudioClipIndex;
+
+        public SoundEffectType Type;
+        public AudioClip AudioClip;
+        public AudioClip[] AudioClips;
+        public SoundEffectSelectionType SelectionType;
+        public float Volume = 1f;
+
+        public void Play()
+        {
+            //Commands.PlaySoundEffect.Publish(this);
+        }
+
+        public void Play(AudioSource audioSource)
+        {
+            switch (Type)
+            {
+                case SoundEffectType.Single:
+                    audioSource.PlayOneShot(AudioClip, Volume);
+                    break;
+                case SoundEffectType.Multiple:
+                    PlayMultiple(audioSource);
+                    break;
+                default:
+                    throw new Exception("Not supported SoundEffectType: " + Type);
+            }
+        }
+
+        private void PlayMultiple(AudioSource audioSource)
+        {
+            switch (SelectionType)
+            {
+                case SoundEffectSelectionType.Random:
+                    AudioClip randomAudioClip = RandomUtil.NextItem(AudioClips);
+                    audioSource.PlayOneShot(randomAudioClip, Volume);
+                    break;
+                case SoundEffectSelectionType.Sequential:
+                    AudioClip nextAudioClip = AudioClips[_nextAudioClipIndex % AudioClips.Length];
+                    _nextAudioClipIndex++;
+                    audioSource.PlayOneShot(nextAudioClip, Volume);
+                    break;
+                default:
+                    throw new Exception("Not supported SelectionType: " + SelectionType);
+            }
+        }
+    }
+
+    public enum SoundEffectType
+    {
+        Single,
+        Multiple
+    }
+
+    public enum SoundEffectSelectionType
+    {
+        Random,
+        Sequential
+    }
+}
