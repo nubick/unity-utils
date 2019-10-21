566c4e6a2859724516551e7e7c957adbf58f3a60
Oleg Andrushko <nubick@gmail.com>
2019-10-21 17:27:28 +0700

    CallMethodScript: Tests dependency is removed.

--- sources/Assets/Scripts/Utils/Editor/CallMethodScriptEditor.cs
+++ sources/Assets/Scripts/Utils/Editor/CallMethodScriptEditor.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Assets.Scripts.Tests;
 using Assets.Scripts.Utils;
 using UnityEditor;
 using UnityEngine;
